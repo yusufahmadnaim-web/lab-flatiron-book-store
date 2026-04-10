02269f0f6b7cc751af542b31f6b1338f698bab2c
ahmadyusuf <yusufahmadnaim@gmail.com>
2026-04-11 00:42:31 +0300

    update book list and header title; remove placeholder element

--- index.js
+++ index.js
@@ -45,3 +45,35 @@ const bookStore = {
 
 // Write your code here!
 
+//DOM updates
+window.addEventListener('DOMContentLoaded', () => {
+    const bookStoreTitle = document.getElementById('header');
+    if (bookStoreTitle) {
+        bookStoreTitle.textContent = bookStore.name;
+    }
+
+    const bookList = document.getElementById('book-list');
+    if (bookList && Array.isArray(bookStore.books)){
+        bookStore.books.forEach((book) =>{
+            const bookContainer = document.createElement('li');
+
+            const bookTitle = document.createElement('h3');
+            bookTitle.textContent = book.title;
+
+            const bookAuthor = document.createElement('p');
+            bookAuthor.textContent = book.author;
+
+            const bookImage = document.createElement('img');
+            bookImage.src = book.imageUrl;
+            bookImage.alt = `${book.title} cover`;
+
+            bookContainer.append(bookTitle, bookAuthor,bookImage);
+            bookList.appendChild(bookContainer)
+        });
+    }
+
+    const deleteThis = document.getElementById('delete-this');
+    if (deleteThis) {
+        deleteThis.remove();
+    }
+});
